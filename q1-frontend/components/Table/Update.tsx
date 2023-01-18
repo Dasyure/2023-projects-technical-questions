b199be712a9ca7cad778606e5e7d0c188f8d820d
Daysure <sandeep.das@student.unsw.edu.au>
2023-01-18 16:54:12 +1100

    q1 part 1 complete

---
+++ q1-frontend/components/Table/Update.tsx
@@ -0,0 +1,24 @@
+import styles from "./Table.module.css";
+import { AlertUpdate } from "./Table";
+
+const UpdateRow = (props: {update: AlertUpdate}) => {
+    console.log(props.update);
+    return (
+        <div className={styles.rowUpdate}>
+            <div>{props.update.update}</div>
+            <div className={styles.rowUpdateDate}>{props.update.date}</div>
+        </div>
+    )
+}
+
+const Update = (props: {updates: AlertUpdate[]}) => {
+    const rows = [];
+    for (const update of props.updates) rows.push(<UpdateRow update={update}/>);
+    return (
+    <div className={styles.updateContainer}>
+        {rows}
+    </div>
+    )
+}
+
+export default Update;
